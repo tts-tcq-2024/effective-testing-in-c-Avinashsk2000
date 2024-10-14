40f2e914c9dcf10d0d181ea214015d9f06f476f7
AVINASH SHIVANAND KUMBAR <84611916+Avinashsk2000@users.noreply.github.com>
2024-10-14 10:55:50 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -8,16 +8,6 @@ int printColorMap() {
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            
-            // Add an assertion to check if the correct minor color is used
-            assert(minorColor[j] != NULL);  // Ensure j indexes into minorColor properly
-            
-            // Check if major and minor colors form the correct pairs
-            if (i == 1 && j == 1) {  // Expect Red and Orange at index 6
-                assert(strcmp(majorColor[i], "Red") == 0);
-                assert(strcmp(minorColor[j], "Orange") == 0);  // This will fail due to the bug
-            }
-                        
             printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
         }
     }
