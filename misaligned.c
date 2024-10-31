309706a8b10773f0fab40731013ebbd6a5f3091b
AVINASH SHIVANAND KUMBAR <84611916+Avinashsk2000@users.noreply.github.com>
2024-10-31 11:10:59 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -45,7 +45,6 @@ int printColorMap() {
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            // Bug here: using minorColor[i] instead of minorColor[j]
             my_printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]); 
         }
     }
